09cd4115340ab54b5e3340e1d479b06610eca68f
JacobCoberly <112647193+JacobCoberly@users.noreply.github.com>
2022-12-12 21:21:20 -0600

    Updated testCode.py
    Added lines to testCode.py to better represent the parser's capabilities. Added a dictionary, while loop, boolean value, and comments to the code.

--- testCode.py
+++ testCode.py
@@ -2,13 +2,18 @@ def myFunc(var1, var2):
     if var1 > var2:
         return var1
     else:
-        return var2
-
+        return
+#Test1
 def main():
-    var1 = "abc"
+    var1 = "abc" #Test 2
     var2 = 4
     for i in range(0,10):
         var2 += i
         print(myFunc(var1, var2+1))
+
+def new_func():
+    stuff = {1:a}
+    while True:
+        say('how curious')
 
 main()
